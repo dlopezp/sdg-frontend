9754a1cbd84ada96bbbeda75b8db6c40919a32b3
Daniel López <dlopezp.dev@gmail.com>
2024-03-11 21:03:55 +0100

    Add e2e test for filter serialization

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,4 +4,11 @@ describe("App should", () => {
     cy.contains("Europe").click();
     cy.url().should("include", "/europe");
   });
+  it("serialize filter on URL", () => {
+    cy.visit("/");
+    cy.get("input").type(12345);
+    cy.get("select").select("<");
+    cy.url({ decode: true }).should("contain", "operator=<");
+    cy.url().should("contain", "population=12345");
+  });
 });
